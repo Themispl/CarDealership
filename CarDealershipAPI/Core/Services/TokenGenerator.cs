2f3a1f62cb9f929578377b59d6155ae88ba103c6
Themispl <themis.pilarinos@gmail.com>
2025-01-03 21:40:07 +0200

    jwt

---
+++ CarDealershipAPI/Core/Services/TokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CarDealershipAPI.Core.Services
+{
+    public class TokenGenerator
+    {
+        public string GenerateToken( string email)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "US2BlUEkNFMy8yl0t6subj3cJKhAm7kQ7Asg7-mSwq0"u8.ToArray();
+
+            var claims = new List<Claim>
+            {
+                //new(JwtRegisteredClaimNames.Sub, fullname.ToString()),
+                 new(JwtRegisteredClaimNames.Email, email.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "https://localhost:7129",
+                Audience = "http://localhost:4200/",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- CarDealershipAPI/Program.cs
+++ CarDealershipAPI/Program.cs
@@ -3,11 +3,34 @@ using CarDealershipAPI.Core.Mappers;
 using CarDealershipAPI.Core.Services;
 using CarDealershipAPI.Data;
 using CarDealershipAPI.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["JwtSettings: Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+});
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
@@ -21,9 +44,24 @@ builder.Services.AddScoped<ICarCategoryService, CarCategoryService>();
 builder.Services.AddScoped<ICarTypeService, CarTypeService>();
 builder.Services.AddScoped<ICarCategoryMapper, CarCategoryMapper>();
 builder.Services.AddScoped<ICarTypeMapper, CarTypeMapper>();
+builder.Services.AddSingleton<TokenGenerator>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowAnyOrigin());
+});
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AngularClient",
+        b => b.WithOrigins("http://localhost:4200") // Assuming Angular runs on localhost:4200
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -35,9 +73,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapPost("/login", (LoginRequest request, TokenGenerator tokenGenerator) =>
+{
+    return new
+    {
+        access_token = tokenGenerator.GenerateToken(request.Email)
+    };
+});
 app.Run();
